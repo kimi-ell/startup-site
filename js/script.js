72dd9e3c1f39c73583c61b83de9f972d71379d5c
kimi-ell <60607789+kimi-ell@users.noreply.github.com>
2020-06-28 12:26:52 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
                 }
             },
             {
-                breakpoint: 375,
+                breakpoint: 380,
                 settings: {
                     slidesToShow: 1
                 }
